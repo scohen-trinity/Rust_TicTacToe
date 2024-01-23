b0b5a56fbc6fa4729d27ccb441b00fc7b0bd68ca
scohen-trinity <scohen@trinity.edu>
2024-01-23 17:19:59 -0600

    changed hardcoded logic for checking win conditions to looping through a nested array of win conditions

--- src/main.rs
+++ src/main.rs
@@ -88,31 +88,23 @@ fn display_grid(grid: &Grid) {
 }
 
 fn check_grid(grid: &Grid) -> bool {
-    if &grid.positions[0] != &Marker::B && &grid.positions[0] == &grid.positions[1] && &grid.positions[1] == &grid.positions[2] {
-        println!("{:?} won!", &grid.positions[0]);
-        return false;
-    } else if &grid.positions[3] != &Marker::B && &grid.positions[3] == &grid.positions[4] && &grid.positions[4] == &grid.positions[5] {
-        println!("{:?} won!", &grid.positions[3]);
-        return false;
-    } else if &grid.positions[6] != &Marker::B && &grid.positions[6] == &grid.positions[7] && &grid.positions[7] == &grid.positions[8] {
-        println!("{:?} won!", &grid.positions[6]);
-        return false;
-    } else if &grid.positions[0] != &Marker::B && &grid.positions[0] == &grid.positions[3] && &grid.positions[3] == &grid.positions[6] {
-        println!("{:?} won!", &grid.positions[0]);
-        return false;
-    } else if &grid.positions[1] != &Marker::B && &grid.positions[1] == &grid.positions[4] && &grid.positions[4] == &grid.positions[7] {
-        println!("{:?} won!", &grid.positions[1]);
-        return false;
-    } else if &grid.positions[2] != &Marker::B && &grid.positions[2] == &grid.positions[5] && &grid.positions[5] == &grid.positions[8] {
-        println!("{:?} won!", &grid.positions[2]);
-        return false;
-    } else if &grid.positions[0] != &Marker::B && &grid.positions[0] == &grid.positions[4] && &grid.positions[4] == &grid.positions[8] {
-        println!("{:?} won!", &grid.positions[0]);
-        return false;
-    } else if &grid.positions[2] != &Marker::B && &grid.positions[2] == &grid.positions[4] && &grid.positions[4] == &grid.positions[6] {
-        println!("{:?} won!", &grid.positions[2]);
-        return false;
-    } 
+    let winning_combos: [[usize; 3]; 8] = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for element in &winning_combos {
+        if &grid.positions[element[0]] != &Marker::B && &grid.positions[element[0]] == &grid.positions[element[1]] && &grid.positions[element[1]] == &grid.positions[element[2]] {
+            println!("{:?} won!", &grid.positions[element[0]]);
+            return false;
+        }
+    }
 
     true
 }
